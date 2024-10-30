e097f3de602d774569716ad6436c5c75f5d7e12a
Cristian1308 <>
2024-10-30 12:26:32 -0500

    asdad

--- DS2/app.js
+++ DS2/app.js
@@ -137,7 +137,7 @@ function generarBoleto(qrURL, idBoleto, nombre) {
 
     // Generar el boleto como imagen en formato JPG y convertirla en URL temporal
     html2canvas(ticketElement, { useCORS: true, scale: 2 }).then(function (canvas) {
-      boletoImageURL = canvas.toDataURL('image/jpeg', 1.0); // Convertir el boleto a una URL temporal en JPG
+      boletoImageURL = canvas.toDataURL('image/png', 2.0); // Convertir el boleto a una URL temporal en JPG
 
       // Copiar la URL de la imagen al portapapeles automáticamente (opcional)
       copiarURLAlPortapapeles(boletoImageURL);
